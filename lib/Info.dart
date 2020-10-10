e76ec277ac1658b9968e11e4f01a18eaa9499adc
Zeeshan-progs <md.zeeshaniqbal7277@gmail.com>
2020-10-11 01:37:18 +0530

    Update Info Page

--- lib/Info.dart
+++ lib/Info.dart
@@ -123,9 +123,10 @@ List<PlanetInfo> planets = [
       orbitalPeriod: '12 years',
       radius: '69,911  km',
       mass: '1.898 × 10^27 kg',
+      iconImage: 'assets/jupiter.png',
+
       gifs: 'assets/gifs/jupiter.gif',
       specification: ' Largest / Biggest Planet',
-      iconImage: 'assets/jupiter.png',
       background: 'assets/background/jupiter.png',
       description:
           "The fifth planet from the sun, Jupiter is a giant gas world that is the most massive planet in our solar system — more than twice as massive as all the other planets combined, according to NASA. Its swirling clouds are colorful due to different types of trace gases. And a major feature in its swirling clouds is the Great Red Spot, a giant storm more than 10,000 miles wide. It has raged at more than 400 mph for the last 150 years, at least. Jupiter has a strong magnetic field, and with 75 moons, it looks a bit like a miniature solar system.",
