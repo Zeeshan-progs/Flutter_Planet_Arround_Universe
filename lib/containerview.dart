3f00b1f46da9625648534391707103caebb7ad28
Zeeshan-progs <md.zeeshaniqbal7277@gmail.com>
2020-10-11 01:35:26 +0530

    Update Home Page

--- lib/containerview.dart
+++ lib/containerview.dart
@@ -9,135 +9,168 @@ class ContainerView extends StatefulWidget {
 }
 
 class _ContainerViewState extends State<ContainerView> {
-  PageController _controller;
-  int _page = 0;
-  @override
-  void initState() {
-    super.initState();
-    _controller = PageController();
-  }
-
+  int currentPage = 0;
   void onPageChanged(int page) {
     setState(() {
-      _page = page;
+      currentPage = page;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SafeArea(
-        child: PageView(
-          children: [
-            Container(
-              child: Stack(
-                children: [
-                  Image.asset(
-                    'assets/gifs/background.gif',
-                    height: 800,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
+      body: PageView(
+        children: [
+          Container(
+            child: Stack(
+              children: [
+                Image.asset(
+                  'assets/background.jpg',
+                  height: size.height,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+                Positioned(
+                  top: 60,
+                  left: 20,
+                  child: RotatedBox(
+                    quarterTurns: 1,
+                    child: Text(
+                      planets[currentPage].name.toUpperCase(),
+                      style: TextStyle(
+                        fontSize: 70,
+                        decoration: TextDecoration.overline,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
                   ),
-                  PageView.builder(
-                    itemCount: planets.length,
-                    itemBuilder: (context, index) {
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            PageRouteBuilder(
-                              pageBuilder: (context, a, b) => ExpandedView(
-                                planetInfo: planets[index],
-                              ),
-                            ),
-                          );
-                        },
-                        child: Container(
-                          padding: EdgeInsets.only(left: 20, right: 20),
-                          child: Stack(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  RotatedBox(
-                                    quarterTurns: 1,
-                                    child: Text(
-                                      planets[index].name.toUpperCase(),
-                                      style: TextStyle(
-                                        fontSize: 70,
-                                        decoration: TextDecoration.overline,
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w900,
-                                      ),
-                                    ),
-                                  ),
-                                  Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: [
-                                      SizedBox(height: 50),
-                                      Text(
-                                        'Distance From Sun ',
-                                        style: TextStyle(
-                                          fontFamily: 'kaushan script',
-                                          fontSize: 20,
-                                        ),
-                                      ),
-                                      Text(
-                                        planets[index].distance,
-                                        style: TextStyle(
-                                          fontSize: 30,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                      Text(
-                                        'Radius ',
-                                        style: TextStyle(
-                                          fontFamily: 'kaushan script',
-                                          fontSize: 20,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                      Text(
-                                        planets[index].radius,
-                                        style: TextStyle(
-                                          fontSize: 30,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                              Positioned(
-                                top: 230,
-                                left: 0,
-                                child: Image.asset(
-                                  planets[index].iconImage,
-                                  height: 300,
-                                  width: 300,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              Positioned(
-                                bottom: 50,
-                                right: 0,
-                                left: 0,
-                                child: Center(
-                                  child: Text('Click to know more'),
-                                ),
-                              ),
-                            ],
-                          ),
+                ),
+                Positioned(
+                  right: 20,
+                  top: 70,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 50),
+                      DisplayText(
+                        text: 'Distance from Sun',
+                        fontSize: 27,
+                      ),
+                      Text(
+                        planets[currentPage].distance,
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
                         ),
-                      );
-                    },
+                      ),
+                      DisplayText(
+                        fontSize: 27,
+                        text: 'Radius',
+                      ),
+                      Text(
+                        planets[currentPage].radius,
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                PageView.builder(
+                  onPageChanged: onPageChanged,
+                  itemCount: planets.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          PageRouteBuilder(
+                            pageBuilder: (context, a, b) =>
+                                ExpandedView(
+                              planetInfo: planets[index],
+                            ),
+                          ),
+                        );
+                      },
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            bottom: -230,
+                            left: 0,
+                            child: Image.asset(
+                              planets[index].iconImage,
+                              height: 500,
+                              width: 400,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                Positioned(
+                  right: 10,
+                  bottom: 150,
+                  child: Column(
+                    children: List.generate(
+                      planets.length,
+                      (index) => buildDot(index),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 50,
+                  right: 0,
+                  left: 0,
+                  child: Center(
+                    child: Text('Click to know more'),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  AnimatedContainer buildDot(int index) {
+    return AnimatedContainer(
+      height: 10,
+      width: 10,
+      margin: EdgeInsets.symmetric(vertical: 5),
+      duration: Duration(milliseconds: 287),
+      curve: Curves.bounceInOut,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.yellow),
+        color:
+            currentPage == index ? Colors.white : Colors.transparent,
+        borderRadius: BorderRadius.circular(30),
+      ),
+    );
+  }
+}
+
+class DisplayText extends StatelessWidget {
+  const DisplayText({
+    Key key,
+    this.fontSize,
+    this.text,
+  }) : super(key: key);
+
+  final double fontSize;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontFamily: 'kaushan script',
+        fontSize: fontSize,
       ),
     );
   }
